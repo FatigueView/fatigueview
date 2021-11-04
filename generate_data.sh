9593f5231549b9fa69a9df2c4e00d6c0baab7ff8
FatigueView <79316630+fatigueviewdotorg@users.noreply.github.com>
2021-11-04 15:33:02 +0800

    Add files via upload

---
+++ generate_data.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+cd Byrnes2018OUD
+/home/users/zhenyu.yang/anaconda3/bin/python3.8 generate_dataset_from_FatigueView.py
+
+
+cd ../Cheng2019AOD
+/home/users/zhenyu.yang/anaconda3/bin/python3.8 generate_dataset_from_FatigueView.py
+
+
+cd ../Drutarovsky2014EBD
+/home/users/zhenyu.yang/anaconda3/bin/python3.8 generate_dataset_from_FatigueView_withRAFT.py
+
+
+cd ../Jo2014DDD
+/home/users/zhenyu.yang/anaconda3/bin/python3.8 generate_dataset_from_FatigueView.py
+
+
+cd ../Massoz2016TUM
+/home/users/zhenyu.yang/anaconda3/bin/python3.8 generate_dataset_from_FatigueView.py
+
+
+cd ../Shi2019TAG
+/home/users/zhenyu.yang/anaconda3/bin/python3.8 generate_dataset_from_FatigueView.py
+
+cd ../Song2014ECD
+/home/users/zhenyu.yang/anaconda3/bin/python3.8 generate_dataset_from_FatigueView.py
+
+
+cd ../Soukupova2016REB
+/home/users/zhenyu.yang/anaconda3/bin/python3.8 generate_dataset_from_FatigueView.py
